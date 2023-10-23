f663fadfd98d3447a01f369c5aa3d78d2050200a
gmccar05 <126704188+gmccar05@users.noreply.github.com>
2023-10-22 21:52:23 -0400

    Did some testing with joints and gravity

---
+++ SirDentist/Assets/Gavin's Folder/Scripts/MouseToWeaponScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseToWeaponScript : MonoBehaviour
+{
+    GameObject weaponForcePoint;
+    //RigidBody2D weaponrb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+    }
+}
