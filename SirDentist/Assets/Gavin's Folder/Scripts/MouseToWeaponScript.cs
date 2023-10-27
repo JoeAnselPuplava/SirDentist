a1cb8b85e8c05b6cabc8607cdd6dc31e264af7f9
gmccar05 <126704188+gmccar05@users.noreply.github.com>
2023-10-27 16:04:58 -0400

    Updated the flail a bit
    Changed the way the flail moves. It still's pretty weird but its somewhat better

--- SirDentist/Assets/Gavin's Folder/Scripts/MouseToWeaponScript.cs
+++ SirDentist/Assets/Gavin's Folder/Scripts/MouseToWeaponScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MouseToWeaponScript : MonoBehaviour
@@ -9,11 +10,18 @@ public class MouseToWeaponScript : MonoBehaviour
     float speed;
     public GameObject weaponForcePoint;
     Rigidbody2D rb;
+
+    public Rigidbody2D ball;
     Vector2 mousePos;
+    Vector2 ScreenCenter;
+    
+    
     //RigidBody2D weaponrb;s
     // Start is called before the first frame update
     void Start()
     {
+        ScreenCenter.x = Screen.width/2;
+        ScreenCenter.y = Screen.height/2;
         rb = weaponForcePoint.GetComponent<Rigidbody2D>();
         
     }
@@ -21,12 +29,17 @@ public class MouseToWeaponScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Debug.Log(mousePos - ScreenCenter);
         mousePos = Input.mousePosition;
         Vector2 ballPos = rb.position;
-        //rb.velocity+= new Vector2(4,4);
-        rb.AddForce(mousePos * thrust);
+        rb.velocity+= new Vector2((mousePos.x - ScreenCenter.x) * thrust,(mousePos.y - ScreenCenter.y)* thrust);
+        ball.velocity+= new Vector2((mousePos.x - ScreenCenter.x) / 150,(mousePos.y - ScreenCenter.y)/ 150);
+        //rb.AddForce((mousePos - ScreenCenter) * thrust);
         if(rb.velocity.magnitude > maxspeed){
             rb.velocity = Vector2.ClampMagnitude(rb.velocity,maxspeed);
+        }
+        if(ball.velocity.magnitude > maxspeed){
+            ball.velocity = Vector2.ClampMagnitude(rb.velocity,maxspeed);
         }
     }
     
