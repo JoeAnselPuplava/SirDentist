4d9047d0c6cb58b2a9971809af555de462826b94
Joe-Ansel Puplava <Joe-Ansel.Puplava@tufts.edu>
2023-11-06 12:55:12 -0500

    Made basic enemy Damage script
    Made the basic enemy damage script as well made some adjustments to the enemy movement

---
+++ SirDentist/Assets/Joe's Folder/Enemy Code/EnemyDamage.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+
+    private GameHandler gameHandler;
+    public int damage = 10;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            animator.SetBool("hasHit", true);
+        }
+
+        else if (other.gameObject.tag == "Flail")
+        {
+
+        }
+
+        else if (other.gameObject.tag == "Sword")
+        {
+
+        }
+    }
+}
---
+++ SirDentist/Assets/Joe's Folder/SwordSTuff/SwordMovement.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordMovement : MonoBehaviour
+{
+    public float thrust = 400.0f;
+    public float maxspeed = 3f;
+    float speed;
+    public GameObject weaponForcePoint;
+    Rigidbody2D rb;
+
+    public Rigidbody2D ball;
+    Vector2 mousePos;
+    Vector2 ScreenCenter;
+
+    public float maxRadius = 10f;
+    public Transform targetPoint;
+
+    //RigidBody2D weaponrb;s
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScreenCenter.x = Screen.width / 2;
+        ScreenCenter.y = Screen.height / 2;
+        rb = weaponForcePoint.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 ballPos = ball.position;
+        Vector2 trueballPos = ballPos;
+        Vector2 truePos = mousePosition;
+        if (targetPoint != null)
+        {
+            Vector2 targetPosition = targetPoint.position;
+            truePos = truePos - targetPosition;
+            trueballPos = trueballPos - targetPosition;
+            //Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector2 direction = targetPosition - mousePosition;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            // Ensure the angle is positive (between 0 and 360 degrees)
+            if (angle < 0)
+            {
+                angle += 360f;
+            }
+
+            Debug.Log("Angle: " + angle);
+
+
+
+            float distance = Vector2.Distance(targetPosition, mousePosition);
+            if (distance > maxRadius)
+            {
+                float angleRadians = angle * Mathf.Deg2Rad;
+                float clampedX = maxRadius * Mathf.Cos(angleRadians);
+                float clampedY = maxRadius * Mathf.Sin(angleRadians);
+                truePos = new Vector2(-clampedX, -clampedY);
+            }
+        }
+
+        Debug.Log(mousePosition);
+        rb.velocity += (truePos - trueballPos);
+        ball.velocity += (truePos - trueballPos);
+
+        //rb.AddForce((truePos - trueballPos) * thrust);
+        //ball.AddForce((truePos - trueballPos)* thrust);
+
+    }
+
+}
