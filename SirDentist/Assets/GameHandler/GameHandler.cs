39013f1f64055b8984c4fe1de25f85fce3118b4d
VarleenBiketi <ayumabiketi96@gmail.com>
2023-11-11 19:20:29 -0500

    Endlose scene

--- SirDentist/Assets/GameHandler/GameHandler.cs
+++ SirDentist/Assets/GameHandler/GameHandler.cs
@@ -70,14 +70,14 @@ public class GameHandler : MonoBehaviour {
       }
 
       public void playerDies(){
-            player.GetComponent<PlayerHurt>().playerDead();       //play Death animation
+            //player.GetComponent<PlayerHurt>().playerDead();       //play Death animation
             lastLevelDied = sceneName;       //allows replaying the Level where you died
             StartCoroutine(DeathPause());
       }
 
       IEnumerator DeathPause(){
-            player.GetComponent<PlayerMove>().isAlive = false;
-            player.GetComponent<PlayerJump>().isAlive = false;
+            //player.GetComponent<PlayerMove>().isAlive = false;
+            //player.GetComponent<PlayerJump>().isAlive = false;
             yield return new WaitForSeconds(1.0f);
             SceneManager.LoadScene("EndLose");
       }
