ccde5c056e958ff2ab82ae486d4c370bd0d5dbef
gmccar05 <126704188+gmccar05@users.noreply.github.com>
2023-11-12 18:35:44 -0500

    Text system now works and a new main player v0.02 has been created
    text system is currently only in its own scene and the new player is seperate from the weapon.

--- SirDentist/Assets/Gavin's Folder/Scripts/DialogueManager.cs
+++ SirDentist/Assets/Gavin's Folder/Scripts/DialogueManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
     public Queue<string> sentences;
+
+    public TMP_Text title;
+    public TMP_Text textbox;
+
+    public GameObject dialogueBox;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,9 +18,10 @@ public class DialogueManager : MonoBehaviour
     }
 
     public void StartDialogue(Dialogue dialogue){
-        Debug.Log("Starting convo with" + dialogue.title);
+        dialogueBox.SetActive(true);
+        title.text = dialogue.title;
         sentences.Clear();
-        foreach (string sentence in sentences){
+        foreach (string sentence in dialogue.sentences){
             sentences.Enqueue(sentence);
         }
         DisplayNextSentence();
@@ -23,10 +30,11 @@ public class DialogueManager : MonoBehaviour
     public void DisplayNextSentence(){
         if(sentences.Count == 0){
             EndDialogue();
+            dialogueBox.SetActive(false);
             return;
         }
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        textbox.text = sentence;
     }
     public void EndDialogue(){
         Debug.Log("End of Convo");
