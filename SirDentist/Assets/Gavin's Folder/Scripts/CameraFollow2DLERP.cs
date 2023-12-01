485d969b685e9b8e3c845818badd1a7535fa0125
gmccar05 <126704188+gmccar05@users.noreply.github.com>
2023-12-01 18:15:10 -0500

    New level tutorial

--- SirDentist/Assets/Gavin's Folder/Scripts/CameraFollow2DLERP.cs
+++ SirDentist/Assets/Gavin's Folder/Scripts/CameraFollow2DLERP.cs
@@ -7,12 +7,14 @@ public class CameraFollow2DLERP : MonoBehaviour
     public GameObject target;
     public float camSpeed = 4.0f;
 
+    public float up = 4f;
+
     void Start(){
         target = GameObject.FindWithTag("Player");
     }
 
     void FixedUpdate () {
         Vector2 pos = Vector2.Lerp ((Vector2)transform.position, (Vector2)target.transform.position, camSpeed * Time.fixedDeltaTime);
-        transform.position = new Vector3 (pos.x, pos.y, transform.position.z);
+        transform.position = new Vector3 (pos.x, pos.y+up, transform.position.z);
     }
 }
