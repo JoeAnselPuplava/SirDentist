9b08e7214a5abde6ce7a930364dca49b5098a775
Joe-Ansel Puplava <Joe-Ansel.Puplava@tufts.edu>
2023-12-08 18:00:11 -0500

    Update BreakableWall.cs

--- SirDentist/Assets/scripts/BreakableWall.cs
+++ SirDentist/Assets/scripts/BreakableWall.cs
@@ -12,15 +12,14 @@ public class BreakableWall : MonoBehaviour
     //public GameObject boxColliderObj; // a child collider that can be turned off
     private Renderer myRend;
     private Color defaultColor;
-    private Collider myCollider;
-
+    private BoxCollider2D myCollider;
+    private bool once = true;
     public GameObject art;
 
     void Start()
     {
         anim = GetComponent<Animator>();
-        //boxColliderObj.SetActive(true);
-        myCollider = GetComponent<Collider>();
+        myCollider = gameObject.GetComponent<BoxCollider2D>();
         myRend = gameObject.GetComponentInChildren<Renderer>();
         defaultColor = myRend.material.color;
     }
@@ -35,19 +34,13 @@ public class BreakableWall : MonoBehaviour
         else if (hitNum == 1)
         {
             anim.SetTrigger("half_break");
-            print("half_break");
-            //anim.SetBool("wallHalf", true);
-            //anim.SetBool("wallGone", false);
         }
-        else if (hitNum <= 0)
+        else if (hitNum <= 0 && once)
         {
+            once = false;
             anim.SetTrigger("break");
-            //anim.SetBool("break", true);
-            print("break");
-            //anim.SetBool("wallGone", true);
-            //boxColliderObj.SetActive(false);
             art.SetActive(false);
-            myCollider.enabled = !myCollider.enabled;
+            myCollider.enabled = false;
 
         }
     }
@@ -57,19 +50,24 @@ public class BreakableWall : MonoBehaviour
         // this is the function that the player attack script would access
         if (hitNum > 0)
             //if (!breakSFX.isPlaying) { breakSFX.Play(); }
-        if (hitNum == 2) { //anim.SetTrigger("break"); 
-    }
+            if (hitNum == 2)
+            { //anim.SetTrigger("break"); 
+            }
         //else if (hitNum == 1) { anim.SetTrigger("cutHalf"); }
         StartCoroutine(wallHitReturn());
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        print("hit");
-        if (collision.gameObject.tag == "Flail")
+        if (collision.gameObject.tag == "Flail" || collision.gameObject.tag == "Sword")
         {
-            hitNum--;
+            hitByWeapon();
         }
+    }
+
+    public void hitByWeapon()
+    {
+        hitNum--;
     }
 
     IEnumerator wallHitReturn()
