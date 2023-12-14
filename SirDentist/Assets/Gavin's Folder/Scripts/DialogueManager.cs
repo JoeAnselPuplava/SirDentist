e6e681c569756f5263a58ac6846d0c84ea7ce0a5
gmccar05 <126704188+gmccar05@users.noreply.github.com>
2023-12-14 17:42:02 -0500

    enquee test

--- SirDentist/Assets/Gavin's Folder/Scripts/DialogueManager.cs
+++ SirDentist/Assets/Gavin's Folder/Scripts/DialogueManager.cs
@@ -33,6 +33,7 @@ public class DialogueManager : MonoBehaviour
         //oldcameradist = camerascript.up;
         //camerascript.up = 0;
         lines.Enqueue("Test");
+        /*
         foreach (string sentence in dialogue.sentences){
             lines.Enqueue(sentence);
         }
@@ -40,6 +41,7 @@ public class DialogueManager : MonoBehaviour
             titles.Enqueue(title);
         }
         DisplayNextSentence();
+        */
     }
 
     public void DisplayNextSentence(){
@@ -60,3 +62,80 @@ public class DialogueManager : MonoBehaviour
         //camerascript.up = oldcameradist;
     }
 }
+
+/*
+
+public string[] title;
+    public string[] lines;
+    public TMP_Text titlebox;
+    public TMP_Text textbox;
+
+    public GameObject dialogueBox;
+
+    public bool talking = false;
+
+    int curr;
+    int count;
+
+    //public CameraFollow2DLERP camerascript;
+    //float oldcameradist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //camerascript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollow2DLERP>();
+    }
+
+    public void StartDialogue(Dialogue dialogue){
+        //Show box and freeze time
+        dialogueBox.SetActive(true);
+        talking = true;
+        //Shift Camera down
+        //oldcameradist = camerascript.up;
+        //camerascript.up = 0;
+        //lines.Enqueue("Test");
+        count = dialogue.sentences.Length;
+        curr = 1;
+
+        for (int i = 0; i < dialogue.sentences.Length;i++){
+            lines[i] = dialogue.sentences[i];
+            Debug.Log(i);
+        }
+        for (int i = 0; i < dialogue.titles.Length;i++){
+            title[i] = dialogue.titles[i];
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence(){
+        if(count == curr){
+            EndDialogue();
+            //Start time up again close box
+            dialogueBox.SetActive(false);
+            return;
+        }
+        string titl = title[curr];
+        titlebox.text = titl;
+        string sentence = lines[curr];
+        textbox.text = sentence;
+        curr++;
+    }
+    public void EndDialogue(){
+        Debug.Log("End of Convo");
+        talking = false;
+        //camerascript.up = oldcameradist;
+    }
+}
+/*
+    public void DisplayNextSentence(){
+        if(lines.Length == 0){
+            EndDialogue();
+            //Start time up again close box
+            dialogueBox.SetActive(false);
+            return;
+        }
+        string title = title.Dequeue();
+        titlebox.text = title;
+        string sentence = lines.Dequeue();
+        textbox.text = sentence;
+    }
+*/
