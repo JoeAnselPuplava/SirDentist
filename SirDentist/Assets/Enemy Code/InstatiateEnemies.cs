a90f8da6b9a94b87fcc8022357117fb5e5f55d98
Joe-Ansel Puplava <Joe-Ansel.Puplava@tufts.edu>
2023-12-16 23:32:58 -0500

    Finished Stomach Level
    Finished the stomach level with 3 main areas and updated some more enemies. Also made a script that sets active state to true.

---
+++ SirDentist/Assets/Enemy Code/InstatiateEnemies.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstatiateEnemies : MonoBehaviour
+{
+    public List<GameObject> enemiesToSpawn; // List of prefabs to instantiate
+    private bool once = false;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Player") && !once) // Check if the collider belongs to the player
+        {
+            InstantiatePrefabs();
+        }
+    }
+
+    private void InstantiatePrefabs()
+    {
+        Debug.Log("Going to Spawn");
+        if (enemiesToSpawn.Count > 0)
+        {
+            foreach (GameObject prefab in enemiesToSpawn)
+            {
+                prefab.SetActive(true);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Prefab list is empty or spawn point not assigned.");
+        }
+    }
+}
